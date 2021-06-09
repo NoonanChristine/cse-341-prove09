2791cdda365887986ba71d665528fdbe1ba803ad
Christine Noonan <noo19002@byui.edu>
2021-06-09 11:47:59 -0400

    fixing port for heroku

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const PORT = process.env.PORT || 5000;
+
+
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs')
     .use(express.static(__dirname + '/public'))
     .use('/', require('./routes/route'))
-    .listen(5000, () => {
+    .listen(PORT, () => {
         console.log('listening on port 5000')
     });
